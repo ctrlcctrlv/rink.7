0cf2cbfb33da4297f2836855e9952338392256c9
Fredrick Brennan <copypaste@kittens.ph>
2023-04-25 11:52:03 -0400

    cleanup

--- make_manpage.bash
+++ make_manpage.bash
@@ -2,13 +2,10 @@
 set -x
 set -- $@
 # Markdown to manpage, the right™ way!
-[ -z "$1" ] && exit 1 || \
+[ -z "$1" ] && exit 1
 LANG=en_US.UTF-8
 (pandoc --from=gfm --to=man -s -i - | perl -pi -0777 manpagemunge.pl) < "$1" > "$2"
-#
-#
-#
-	#groff -mandoc -Tutf8 -Kutf8
+[ -z "$MANPAGE_DISPLAY" ] || groff -mandoc -Tutf8 -Kutf8 "$2"
 
 # Author: Fredrick R. Brennan <copypaste@kittens.ph>
 # vim: noet sw=4 ts=4
